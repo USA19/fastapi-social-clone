3d366f80946b8516ff075eb771e1d05ae490aa41
Usama Sarfraz <muhamedusama648@gmail.com>
2026-01-19 19:58:20 +0500

    get posts end point added

--- routes/post.py
+++ routes/post.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter
-from schemas.post import PostOutput,CreatePostInput
+from fastapi import APIRouter,Query
+from schemas.post import PostOutput,CreatePostInput,PaginatedPostsResponse
 from fastapi import Depends
 from db.session import get_db  
 from auth.jwt import get_current_user
 from models.post import Post
 from models.user import User
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session,joinedload
+import math
 
 router = APIRouter(prefix="/posts", tags=["Posts"])
 
@@ -25,3 +26,30 @@ def create_post(
     db.refresh(post)
 
     return {"post": post, "message": "Post created successfully"}
+
+@router.get("/", response_model=PaginatedPostsResponse)
+def get_posts(
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, ge=1, le=100),
+    db: Session = Depends(get_db),
+):
+    offset = (page - 1) * limit
+
+    total = db.query(Post).count()
+    totalPages = math.ceil(total / limit) if total > 0 else 1
+    posts = (
+        db.query(Post)
+        .options(joinedload(Post.author))  # prevents N+1 queries
+        .order_by(Post.created_at.desc())
+        .offset(offset)
+        .limit(limit)
+        .all()
+    )
+
+    return {
+        "page": page,
+        "limit": limit,
+        "totalCount": total,
+        "posts": posts,
+        "totalPages": totalPages
+    }
--- schemas/post.py
+++ schemas/post.py
@@ -3,7 +3,7 @@ from typing import Optional
 from enum import Enum
 from uuid import UUID
 from datetime import datetime
-
+from .user import UserOutput
 class PostVisibility(str, Enum):
     public = "public"
     friends = "friends"
@@ -21,7 +21,7 @@ class PostSchema(BaseModel):
     visibility: PostVisibility
     created_at: datetime
     updated_at: datetime
-
+    author: Optional[UserOutput] = None
     class Config:
         from_attributes = True
 
@@ -30,3 +30,10 @@ class PostOutput(BaseModel):
     message:str
     class Config:
       from_attributes = True
+
+class PaginatedPostsResponse(BaseModel):
+    page: int
+    limit: int
+    totalCount: int
+    posts: list[PostSchema]
+    totalPages: int
